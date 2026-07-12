3926f969fa96522f6186c5a32235f623ed167b45
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2026-07-12 19:25:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# base64
+
+Zero-stdlib base64 codec (RFC 4648) for TinyGo and WASM edge binaries
---
+++ base64.go
@@ -0,0 +1,7 @@
+package base64
+
+type Base64 struct {}
+
+func New() *Base64 {
+    return &Base64{}
+}
